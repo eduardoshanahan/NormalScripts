c074f1a4cafcb9fae302ceb72da348cbff08397f
Eduardo Shanahan <contact@eduardoshanahan.com>
2013-02-15 11:47:30 +0000

    Forgot to install Sublime

--- GetUbuntuUsualSuspects.sh
+++ GetUbuntuUsualSuspects.sh
@@ -17,6 +17,7 @@ sudo apt-get -y install chromium-browser
 echo 'Install Sublime'
 add-apt-repository -y ppa:webupd8team/sublime-text-2
 apt-get -y update
+apt-get -y install sublime-text
 
 echo 'Remember to add Sublime`s package manager by pasting the following in the command console (ctrl+\`)'
 echo "import urllib2,os; pf='Package Control.sublime-package'; ipp=sublime.installed_packages_path(); os.makedirs(ipp) if not os.path.exists(ipp) else None; urllib2.install_opener(urllib2.build_opener(urllib2.ProxyHandler())); open(os.path.join(ipp,pf),'wb').write(urllib2.urlopen('http://sublime.wbond.net/'+pf.replace(' ','%20')).read()); print('Please restart Sublime Text to finish installation')"
