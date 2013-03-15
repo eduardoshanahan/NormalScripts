e8a765e2d5c4f802f2ff3e4c0b1c62e07ee0b4ef
Eduardo <contact@eduardoshanahan.com>
2013-03-15 10:31:20 +0000

    Lets remember credentials for a week

--- RememberGit.sh
+++ RememberGit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "Tell Git to remember credentials"
-git config credential.helper "cache --timeout=36000"
+echo "Tell Git to remember credentials for a week"
+git config credential.helper "cache --timeout=604800"
