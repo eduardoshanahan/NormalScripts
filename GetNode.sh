d9c54d19b499a300127e17b725ac156751464376
Eduardo Shanahan <contact@eduardoshanahan.com>
2013-04-27 23:53:40 +0100

    Sorted some access rights and tools in Node

--- GetNode.sh
+++ GetNode.sh
@@ -8,3 +8,10 @@ apt-get install -y nodejs
 echo "Install tools"
 apt-get install -y npm
 npm install -g express
+npm install -g nodemon
+npm install -g mocha
+npm install -g restify
+
+echo "Make sure the access rights are correct"
+sudo chown -R ubuntu.ubuntu ~/tmp
+sudo chown -R ubuntu.ubuntu ~/.npm
